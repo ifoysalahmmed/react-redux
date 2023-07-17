95094d7b38e8407e6fcd803610ba7b9f77cc7c1e
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-18 00:06:29 +0600

    added Reset button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   decrementCounter,
   incrementCounter,
+  resetCounter,
 } from "../services/actions/CounterAction";
 
 const Counter = () => {
@@ -17,12 +18,17 @@ const Counter = () => {
     dispatch(decrementCounter());
   };
 
+  const handleReset = () => {
+    dispatch(resetCounter());
+  };
+
   return (
     <div>
       <h2>Counter App</h2>
       <h3>Count : {count}</h3>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
